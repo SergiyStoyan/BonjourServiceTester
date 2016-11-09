262ba7505ad31ed0341f9c68af83bd57ebc371dd
cliversoft <sergey.stoyan@gmail.com>
2016-11-09 12:42:49 +0200

    .

--- BonjourService/main.cpp
+++ BonjourService/main.cpp
@@ -6,7 +6,8 @@
 #include <string>
 #include <iostream>
 #include <conio.h>
-
+#include <ctime>
+#include <stdexcept>
 
 void DNSSD_API ServiceRegisterReply(
 	DNSServiceRef                       sdRef,
@@ -21,12 +22,50 @@ void DNSSD_API ServiceRegisterReply(
 	printf("ServiceRegisterReply: errorCode=%d, name=%s\r\n", errorCode, name);
 }
 
+BOOL WINAPI HandlerRoutine(_In_ DWORD dwCtrlType)
+{
+	switch (dwCtrlType) {
+	case CTRL_C_EVENT:
+	case CTRL_BREAK_EVENT:
+	case CTRL_CLOSE_EVENT:
+	case CTRL_LOGOFF_EVENT:
+	case CTRL_SHUTDOWN_EVENT:
+		BonjourService::Stop();
+		break;
+	}
+	return FALSE;
+}
+
+int main1(){ 
+	try
+	{
+		Sleep(10000);
+	}
+	catch (...)
+	{
+		
+	}
+	Sleep(10000);
+	return 0;
+}
 
 int main()
 {
-	const char *service_name = "agent123";
+	if (!SetConsoleCtrlHandler(HandlerRoutine, TRUE))
+	{
+		printf("ERROR: Could not SetConsoleCtrlHandler!\r\n");
+		printf("Press any key to exit...\r\n");
+		_getch();
+		return 1;
+	}
+
+	char service_name[64]; 
+	time_t t = time(0); 
+	struct tm now = tm();
+	localtime_s(&now, &t);
+	sprintf_s(service_name, "agent-%d-%d-%d-%d-%d-%d", now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec);
 	DNSServiceErrorType err = BonjourService::Start(service_name, ServiceRegisterReply);
-	if (err != 0) 
+	if (err != 0)
 	{
 		printf("ERROR while registering service: errorCode=%d\r\n", err);
 		printf("Press any key to exit...\r\n");
@@ -40,5 +79,5 @@ int main()
 	BonjourService::Stop();
 	printf("Service stopped...\r\n");
 	Sleep(1000);
-    return 0;
+	return 0;
 }
